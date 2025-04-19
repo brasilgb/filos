27ff2653323a98b84080c7a2df0374df7172a34f
AndersonBrasil <andersonbrasil72@gmail.com>
2025-04-18 21:48:48 -0300

    Push

--- resources/views/filament/admin/logo.blade.php
+++ resources/views/filament/admin/logo.blade.php
@@ -1,19 +1,14 @@
 <div class="flex items-center justify-start gap-2">
-    <svg
-    viewBox="0 0 128 26"
-    xmlns="http://www.w3.org/2000/svg"
-    class="h-full fill-gray-500 dark:fill-gray-400"
->
-    <!-- ... -->
-</svg>
-    {{-- <div>
+    
+    <div>
         <a href="/" class="p-0 m-0">
-            <img src="{{ asset('storage/' . $settings->logo) }}" width="100" />
+            <img src="{{ asset('pernalonga.jpg') }}" width="50" height="40" />
+            {{-- <img src="{{ asset('storage/' . $settings->logo) }}" width="100" /> --}}
         </a>
-    </div> --}}
-    {{-- <div class="pt-1 text-2xl font-bold dark:text-white">
+    </div>
+    {{--<div class="pt-1 text-2xl font-bold dark:text-white">
         <a href="/" class="p-0 m-0">
             {{ $settings->title }}
         </a>
-    </div> --}}
+    </div>--}}
 </div>
