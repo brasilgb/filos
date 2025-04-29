3550cf093ffdb8314ccd6ab93e28e583d04aa7f7
Anderson .Brasil <andersonbrasil72@gmail>
2025-04-29 14:00:35 -0300

    Push

--- app/Livewire/PrintTag.php
+++ app/Livewire/PrintTag.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Company;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Livewire\Component;
 
@@ -23,6 +24,22 @@ class PrintTag extends Component
 
     public function render()
     {
+
         return view('livewire.print-tag');
+    }
+    public function generate(Company $company, Request $request)
+    {
+        $tagi = $request->query('tagi', null);
+        $tagf = $request->query('tagf', null);
+        $nump = $request->query('nump', null);
+        $pdf = Pdf::loadView('livewire.print-tag', [
+            'tagi' => $tagi,
+            'tagf' => $tagf,
+            'nump' => $nump,
+            'company' => $company ?? [],
+        ])->setPaper('A4', 'landscape');
+        $pdf->setOptions(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true]);   
+        
+        return $pdf->stream('tags.pdf');
     }
 }
--- resources/views/livewire/print-tag.blade.php
+++ resources/views/livewire/print-tag.blade.php
@@ -1,42 +1,25 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Imprimir com Parâmetros</title>
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @endif
-</head>
-
-<body>
-    <div class="w-[210mm] h-[297mm] mx-auto">
-        <main class="bg-white ml-[5.7mm] mt-[12.5mm] pb-[12.5mm]">
-            <a href="/admin/tag-page" class="print:hidden">
-                Fechar
-            </a>
-
-            <section class="w-full flex flex-row items-start justify-start flex-wrap flex-grow-1 {{ $nump > 1 ? 'break-after-page' : '' }}">
+    <div class="bg-gray-white text-gray-600 container mx-auto shadow">
+        <a href="/admin/tag-page" class="print:hidden">
+            Fechar
+        </a>
+        <main class="w-[210mm] h-[297mm]">
+            <section class="w-full flex flex-row flex-wrap items-start grow-1 ml-[5.7mm] mt-[12.5mm] pb-[12.5mm]">
                 @for ($x = $tagi; $x <= $tagf; $x++)
-                    <div class="w-[33mm] h-[17mm] py-1.5 border border-gray-2 flex flex-col items-center justify-around">
-                        <div class="text-[12px]">
-                            {{ $company->empresa }}
+                    <div class="w-[33mm] h-[17mm] py-[1.5mm] border flex flex-col items-center justify-start">
+                        <div class="text-xs">
+                            {{ $company->shortname }}
                         </div>
-                        <div class="text-[12px] font-bold">
+                        <div class="text-lg font-bold">
                             {{ $x }}
                         </div>
-                        <div class="text-[12px]">
-                            {{ $company->telefone }}
+                        <div class="text-sm">
+                            {{ $company->telephone }}
                         </div>
                     </div>
                 @endfor
             </section>
         </main>
     </div>
-
-
-
     <script>
         window.onload = function() {
             window.print();
@@ -46,6 +29,3 @@
             };
         };
     </script>
-</body>
-
-</html>
